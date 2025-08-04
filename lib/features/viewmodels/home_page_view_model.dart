1312d7cfca9af1a211b833f9fc76e49cd558877b
kyouga-ones <kyouga.yoshida@onespower.com>
2025-08-04 15:27:54 +0900

    reaftering:task-001:初期プログラムを機能ごとに分割

---
+++ lib/features/viewmodels/home_page_view_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:kakeibo/features/viewmodels/main_view_model.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => MyHomePageState();
+}
