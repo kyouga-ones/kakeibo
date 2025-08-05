d7b58a6c0b6101e0d0fb4c2f4e371fc1c3ad3fd8
kyouga-ones <kyouga.yoshida@onespower.com>
2025-08-05 13:53:46 +0900

    refactor:task-002:views内のファイル名変更

--- lib/ui/components/viewmodels/home_view_model.dart
+++ lib/ui/components/viewmodels/home_view_model.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kakeibo/ui/components/views/home.dart';
+import 'package:kakeibo/ui/components/views/homeScreen.dart';
 
 class HomeViewModel extends StatefulWidget {
   const HomeViewModel({super.key});
@@ -11,6 +11,6 @@ class HomeViewModel extends StatefulWidget {
 class _HomeViewModelState extends State<HomeViewModel> {
   @override
   Widget build(BuildContext context) {
-    return const Home();
+    return const HomeScreen();
   }
 }
